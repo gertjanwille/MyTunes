948713ef4fc953a0de6e08486d1d8da0230920e9
Gert-Jan Wille <gertjanwille.w@gmail.com>
2015-11-11 09:57:11 +0100

    json compiled to html

---
+++ _js/modules/functies.js
@@ -0,0 +1,22 @@
+const html = (strings, ...values) => {
+
+  let str = '';
+
+  if(Array.isArray(strings)){
+    for(let i = 0; i < strings.length; i++){
+      if(strings[i]) str += strings[i];
+      if(values[i]) str += values[i];
+    }
+  }else{
+    str = strings;
+  }
+
+  let doc = new DOMParser().parseFromString(str.trim(), 'text/html');
+
+  return doc.body.firstChild;
+
+};
+
+export default{
+  html
+}
--- _js/script.js
+++ _js/script.js
@@ -1,2 +1,42 @@
-let game = 'hello';
-console.log(game);
+import 'whatwg-fetch';
+import'es6-promise';
+
+import functies from './modules/functies';
+
+let albums;
+
+const init = data =>{
+	albums = data;
+	
+	buildPlaylist(albums);
+};
+
+const loadData = ()=>{
+	fetch('./assets/data/data.json')
+		.then(r =>r.json())
+		.then(({albums})=>init(albums));
+};
+
+const buildPlaylist = albums =>{
+	albums.forEach((item,i)=>{
+		let el = functies.html(`
+			<article class="album">
+				<figure>
+					<div class="cover-container">
+						<img class='cover effect' src="assets/img/covers/${item.cover}.jpg" width="250" height="250">
+						<section class='player hide'>
+							<a class="icon play" href="${item.cover}"></a>
+							<div class="time"></div>
+							<a class="icon buy" href="#"></a>
+						</section>
+					</div>
+					<figcaption>${item.title}</figcaption>
+				</figure>
+			</article>
+			`);
+
+		document.querySelector('.holder').appendChild(el);
+	});
+};
+
+loadData();
