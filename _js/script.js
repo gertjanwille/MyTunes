5ef016117dda85b67c966d2f11911a0703f4c620
Gert-Jan Wille <gertjanwille.w@gmail.com>
2015-11-11 11:23:21 +0100

    added pause & play

--- _js/script.js
+++ _js/script.js
@@ -4,11 +4,22 @@ import'es6-promise';
 import $album from './modules/Export';
 import cover from './modules/Functies';
 
-let albums;
+let albums, current, playing, $audio;
 
 const init = data =>{
 	albums = data;
-	
+
+	document.querySelector('.holder').addEventListener('click', e=>{
+    e.preventDefault();
+    if(e.target.tagName.toLowerCase()=== 'a'){
+    	playing = e.target;
+    	if (e.target.classList.contains('pauseIt')) {
+    		pauseSong();
+    	}else{
+    		playSong(e.target.getAttribute('href'));
+    	};
+    }
+  });
 	
 	$album.buildPlaylist(albums);
 };
@@ -19,7 +30,25 @@ const loadData = ()=>{
 		.then(({albums})=>init(albums));
 };
 
-const mouseOver = e =>{
-	console.log('hello world');
+const playSong = cover =>{
+	current = albums.find(function(item){
+    	return item.cover === cover;
+  	});
+	$audio = document.querySelector('audio');
+	$audio.setAttribute('src', `assets/snd/${cover}.mp3`);
+	if($audio.play){
+		playing.classList.remove('play');
+		playing.classList.add('pause');
+		if(playing.classList.contains('pause')){
+			playing.classList.add('pauseIt');
+		}
+	}
 }
+
+const pauseSong = () =>{
+	$audio.pause()
+	playing.classList.remove('pause', 'pauseIt');
+	playing.classList.add('play');
+};
+
 loadData();
